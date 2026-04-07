6bf13193bc49e9998de0fd9f92af911ba798367f
Samarth <samarthbandekar02@gmail.com>
2026-04-07 22:27:15 +0530

    Initial commit

---
+++ guess the number exercise 20.py
@@ -0,0 +1,19 @@
+num=18
+print("enter a number to guess")
+
+while(True):
+    numberofguess=32
+    input1=int(input())
+    if input1<18:
+        print("guess upper number")
+        continue
+    elif input1>18:
+        print("guess lower number")
+        continue
+    else:
+        print("number is correct")
+        break
+
+        #samarth bandekar
+        #samarthbandekar_
+        #instagram
